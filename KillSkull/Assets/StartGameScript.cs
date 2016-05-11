212e7ef073d1e13a9d5c06bb285f1fcf2bdce56f
Shubham Vadhera <shubhamvadhera@gmail.com>
2016-05-11 15:08:12 -0700

    Intial commit

---
+++ KillSkull/Assets/StartGameScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGameScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PressButton() {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void PressQuitButton()
+    {
+        Application.Quit();
+    }
+}
